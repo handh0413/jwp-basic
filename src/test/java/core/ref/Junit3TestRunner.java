cb3b4d367220210024f799856cd6bfd26cae2a82
donghee <handh0413@naver.com>
2021-03-15 21:03:13 +0900

    "test"로 시작하는 메소드 실행하기

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,19 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
+        
+        for (Method method : clazz.getDeclaredMethods()) {
+        	String methodName = method.getName();
+        	if (methodName.startsWith("test")) {
+        		method.invoke(clazz.newInstance());
+        	}
+        }
     }
 }
