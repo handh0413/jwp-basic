07bcbd2492c5032a33829e26b27158efda13b29d
donghee <handh0413@naver.com>
2021-03-15 20:58:45 +0900

    자바 리플렉션 API를 활용해 클래스 정보 출력하기

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,18 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        
+        for (Field field : clazz.getDeclaredFields()) {
+        	logger.debug("field : {}", field.toString());
+        }
+        
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+        	logger.debug("constructor : {}", constructor.toString());
+        }
+        
+        for (Method method : clazz.getDeclaredMethods()) {
+        	logger.debug("method : {}", method.toString());
+        }
     }
     
     @Test
@@ -27,4 +43,5 @@ public class ReflectionTest {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
     }
+    
 }
