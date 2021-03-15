240b60736cdaee8bc375cf884aca820407a7bda7
donghee <handh0413@naver.com>
2021-03-15 21:15:26 +0900

    생성자가 있는 클래스의인스턴스 생성하기 수정

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,42 +1,23 @@
 package core.ref;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import next.model.User;
-
 public class Junit4TestRunner {
 	private Logger logger = LoggerFactory.getLogger(Junit4TestRunner.class);
-	
-    @Test
-    public void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
 
-        for (Method method : clazz.getDeclaredMethods()) {
-        	if(method.isAnnotationPresent(MyTest.class)) {
-        		method.invoke(clazz.newInstance());
-        	}
-        }
-    }
-    
-    @Test
-    public void userTest() throws Exception {
-    	Class<User> clazz = User.class;
-    	
-    	for(Constructor constructor : clazz.getDeclaredConstructors()) {
-    		logger.debug(constructor.toString());
-    		
-    		String userId = "donghee.han";
-    		String password = "1234";
-    		String name = "동희";
-    		String email = "dongee.han@test.com";
-    		
-    		User user = (User) constructor.newInstance(userId, password, name, email);
-    		logger.debug(user.toString());
-    	}
-    }
+	@Test
+	public void run() throws Exception {
+		Class<Junit4Test> clazz = Junit4Test.class;
+
+		for (Method method : clazz.getDeclaredMethods()) {
+			if (method.isAnnotationPresent(MyTest.class)) {
+				method.invoke(clazz.newInstance());
+			}
+		}
+	}
+
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -12,36 +12,47 @@ import next.model.Question;
 import next.model.User;
 
 public class ReflectionTest {
-    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    @Test
-    public void showClass() {
-        Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
-        
-        for (Field field : clazz.getDeclaredFields()) {
-        	logger.debug("field : {}", field.toString());
-        }
-        
-        for (Constructor constructor : clazz.getDeclaredConstructors()) {
-        	logger.debug("constructor : {}", constructor.toString());
-        }
-        
-        for (Method method : clazz.getDeclaredMethods()) {
-        	logger.debug("method : {}", method.toString());
-        }
-    }
-    
-    @Test
-    public void newInstanceWithConstructorArgs() {
-        Class<User> clazz = User.class;
-        logger.debug(clazz.getName());
-    }
-    
-    @Test
-    public void privateFieldAccess() {
-        Class<Student> clazz = Student.class;
-        logger.debug(clazz.getName());
-    }
-    
+	@Test
+	public void showClass() {
+		Class<Question> clazz = Question.class;
+		logger.debug(clazz.getName());
+
+		for (Field field : clazz.getDeclaredFields()) {
+			logger.debug("field : {}", field.toString());
+		}
+
+		for (Constructor constructor : clazz.getDeclaredConstructors()) {
+			logger.debug("constructor : {}", constructor.toString());
+		}
+
+		for (Method method : clazz.getDeclaredMethods()) {
+			logger.debug("method : {}", method.toString());
+		}
+	}
+
+	@Test
+	public void newInstanceWithConstructorArgs() throws Exception {
+		Class<User> clazz = User.class;
+
+		for (Constructor constructor : clazz.getDeclaredConstructors()) {
+			logger.debug(constructor.toString());
+
+			String userId = "donghee.han";
+			String password = "1234";
+			String name = "동희";
+			String email = "dongee.han@test.com";
+
+			User user = (User) constructor.newInstance(userId, password, name, email);
+			logger.debug(user.toString());
+		}
+	}
+
+	@Test
+	public void privateFieldAccess() {
+		Class<Student> clazz = Student.class;
+		logger.debug(clazz.getName());
+	}
+
 }
